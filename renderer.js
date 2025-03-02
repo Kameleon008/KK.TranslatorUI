3e397fa031d83c45b83029a5d2dd9a0d8fc4c659
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2025-03-02 01:19:01 +0100

    Update

--- renderer.js
+++ renderer.js
@@ -1,13 +1,32 @@
 const { ipcRenderer } = require('electron');
 
-// Debug log to confirm script is running
-console.log("renderer.js is running...");
+// HTML Elements
+const contentContainer = document.getElementById('content')
+const waitingBar = document.getElementById('progressBarContainer')
+const translationContainer = document.getElementById('translationContainer')
+const translationResult = document.getElementById('translation')
 
 // Listen for arguments from main process
 ipcRenderer.on('arguments', async (event, argObject) => {
-    console.log('Received arguments in renderer:', argObject);
 
-    // Ensure UI updates only when data is received
+    await wait(1000);
+    contentContainer.classList.remove('acrylic-background__init');
+
+    const response = await getTranslationResult(argObject);
+    translationResult.innerText = `${response.translatedText || 'N/A'}`;
+
+    translationContainer.classList.remove('translation-container__init');
+    waitingBar.classList.add(`progress-bar-container__hidden`);
+
+    ipcRenderer.send('resize-window', contentContainer.clientWidth + 100, contentContainer.clientHeight + 100);
+});
+
+async function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getTranslationResult(argObject) {
+
 
     const response = await fetch("http://localhost:5000/translate", {
         method: "POST",
@@ -22,17 +41,9 @@ ipcRenderer.on('arguments', async (event, argObject) => {
         headers: { "Content-Type": "application/json" }
     });
 
-    if (!response.ok) {
+    if (response.ok === false) {
         throw new Error('Network response was not ok ' + response.statusText);
     }
-    const data = await response.json(); // Parse the response as JSON
 
-    document.getElementById('username').innerText = `${data.translatedText || 'N/A'}`;
-
-    // Measure the content size
-    let content = document.getElementById('content');
-
-    // Send the content size to the main process
-    ipcRenderer.send('resize-window', content.clientWidth, content.clientHeight);
-});
-
+    return await response.json();;
+}
